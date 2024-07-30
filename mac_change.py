82c423df086269f2e629df1dd86f01e2fe791e10
prof-paradox-773 <76960239+prof-paradox-773@users.noreply.github.com>
2024-07-30 06:15:58 +0530

    Create mac_change.py

---
+++ mac_change.py
@@ -0,0 +1,11 @@
+import subprocess
+interface = input("interface >")
+new_mac =  input("new mac >")
+
+
+
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig",interface,"up"])
+
+
